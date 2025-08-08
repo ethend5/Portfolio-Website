1f4a0a3fd857c3abf44d89d42728a18632ab4f96
ethend5 <ethendhanaraj@gmail.com>
2025-08-08 16:36:01 -0700

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,147 @@
+/**
+* @copyright 2025 Ethen Dhanaraj
+* @license Apache-2.0
+*/
+
+
+/**
+* Components
+*/
+import { ButtonPrimary } from "./Button";
+
+
+const sitemap = [
+ {
+   label: 'Home',
+   href: '#home'
+ },
+ {
+   label: 'About',
+   href: '#about'
+ },
+ {
+   label: 'Projects',
+   href: '#projects'
+ },
+ {
+   label: 'Experience',
+   href: '#experience'
+ },
+ {
+   label: 'Contact me',
+   href: '#contact'
+ }
+];
+
+
+const socials = [
+ {
+   label: 'GitHub',
+   href: 'https://github.com/ethend5'
+ },
+ {
+   label: 'LinkedIn',
+   href: 'https://www.linkedin.com/in/ethen-dhanaraj/'
+ }
+];
+
+
+const Footer = () => {
+   return (
+       <footer className="section">
+           <div className="container">
+
+
+               <div className="lg:grid lg:grid-cols-2">
+
+
+                   <div className="mb-10">
+                       <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
+                           Let&apos;s Work Together Today!
+                       </h2>
+
+
+                       <ButtonPrimary
+                           href="#top"
+                           label="Scroll Up"
+                           icon="arrow_upward"
+                           iconPosition="right"
+                       />
+                   </div>
+
+
+                   <div className="grid grid-cols-2 gap-4 lg:pl-20">
+
+
+                       <div>
+                           <p className="mb-2">Sitemap</p>
+
+
+                           <ul>
+                               {sitemap.map(({ label, href }, key) => (
+                                   <li key={key}>
+                                       <a
+                                           href={href}
+                                           className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                                       >
+                                           {label}
+                                       </a>
+                                   </li>
+                               ))}
+                           </ul>
+                       </div>
+
+
+                       <div>
+                           <p className="mb-2">Socials</p>
+
+
+                           <ul>
+                               {socials.map(({ label, href }, key) => (
+                                   <li key={key}>
+                                       <a
+                                           href={href}
+                                           target="_blank"
+                                           className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                                       >
+                                           {label}
+                                       </a>
+                                   </li>
+                               ))}
+                           </ul>
+                       </div>
+
+
+                   </div>
+
+
+               </div>
+
+
+               <div className="flex items-center justify-between pt-10 mb-8">
+                   <a
+                       href=""
+                       className=""
+                   >
+                       <img  
+                           src="/images/logo.svg"
+                           width={40}
+                           height={40}
+                           alt="Logo"
+                       />
+                   </a>
+
+
+                   <p className="text-zinc-500 text-sm">
+                       &copy; 2025 <span className="text-zinc-200">Ethen Dhanaraj</span>
+                   </p>
+               </div>
+
+
+           </div>
+       </footer>
+   )
+}
+
+
+export default Footer
