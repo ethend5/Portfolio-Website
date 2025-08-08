9c5df34f7af7470dce4e222219a579ec92a18792
ethend5 <ethendhanaraj@gmail.com>
2025-08-08 16:39:34 -0700

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,29 @@
+/**
+* @copyright 2025 Ethen Dhanaraj
+* @license Apache-2.0
+*/
+
+
+/**
+* Node Modules
+*/
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+/**
+* CSS link
+*/
+import './index.css';
+
+
+/**
+* Components
+*/
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
