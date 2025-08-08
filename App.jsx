50e54dcf899ec43d264cfce5f4b8cf61782906e0
ethend5 <ethendhanaraj@gmail.com>
2025-08-08 16:14:35 -0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,41 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skill from "./components/Skill";
+import Project from './components/Project';
+import Experience from './components/Experience';
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import ProjectDetails from './components/ProjectDetails';
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+        <Project />
+        <Experience />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/project/:id" element={<ProjectDetails />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
